c35ad754e050f85955f8d3e519f99dc2309a6377
coltonrobbins73 <coltonrobbins73@gmail.com>
2024-06-16 08:19:18 -0700

    prompt tweaking to disallow all caps formatting in resume summary

--- prompts/resumePrompt.ts
+++ prompts/resumePrompt.ts
@@ -3,9 +3,13 @@ import { dedent } from "ts-dedent";
 export const generateResumePrompt = (resume: string) => dedent(`
     Your task is to extract information from the resume and output in JSON format.
 
-    Extract the name and overall title of the applicant.
-    Also, extract ALL previous work experiences, including company name, job title,
-    and the word for word description of job duties found in the resume.
+    - Extract contact information for the applicant
+    - Also, extract ALL previous work experiences, including company name, job title,
+    and the description of job duties found in the resume.
+    - Ensure that the description of job duties is extracted exactly as it is written in the
+    resume.
+    - Write the applicant's name in title case.
+    - Reformat all company names in title case.
 
     Resume: ${resume}
 
@@ -15,6 +19,7 @@ export const generateResumePrompt = (resume: string) => dedent(`
     Applicant name:
     Applicant title:
     Applicant email:
+    Applicant phone:
     Applicant residence:
     Applicant portfolio website:
     Applicant github:
