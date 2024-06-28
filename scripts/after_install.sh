7a53263ff4e9a46eeca3d7fcae6b3246543bf403
coltonrobbins73 <coltonrobbins73@gmail.com>
2024-06-28 11:07:09 -0700

    changed pathing

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,7 @@
 docker pull coltonrobbins73/career-docs-api:latest
 
 # Get secrets from Secrets Manager
-SECRET_ARN=$(aws secretsmanager describe-secret --secret-id CareerDocsApiEnvVars --query ARN --output text)
+SECRET_ARN=$(aws secretsmanager describe-secret --secret-id CareerDocsApiEnvVariables --query ARN --output text)
 SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ARN" --query SecretString --output text)
 
 # Create a .env file from the secrets
